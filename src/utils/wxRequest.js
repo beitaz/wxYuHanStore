e427e2735b8367ae3c6798ec695bc3f630134f08
beitaz <beitaz@icloud.com>
2018-05-01 16:11:09 +0800

    修改 ajax 请求的 sign 方式为 timestamp + openId + api_secret_key

--- src/utils/wxRequest.js
+++ src/utils/wxRequest.js
@@ -2,20 +2,19 @@ import wepy from 'wepy';
 import util from './util';
 import md5 from './md5';
 import tip from './tip';
-
-const API_SECRET_KEY = 'www.mall.cycle.com'
-const TIMESTAMP = util.getCurrentTime()
-const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
+import { API_SECRET_KEY } from './constant';
 
 const wxRequest = async(params = {}, url) => {
     tip.loading();
-    let data = params.query || {};
-    data.sign = SIGN;
-    data.time = TIMESTAMP;
+    let query = params.query || {};
+    let timestamp = util.getCurrentTime();
+    query.sign = md5.hex_md5((timestamp + query.openId + API_SECRET_KEY).toLowerCase());
+    query.time = timestamp;
+    console.log('Request params.query = ', query);
     let res = await wepy.request({
         url: url,
         method: params.method || 'GET',
-        data: data,
+        data: query,
         header: { 'Content-Type': 'application/json' },
     });
     tip.loaded();
